1df964ab52570b9c93e4478d2636fded5794b53c
sudhanshu <salil221254@gmail.com>
2024-12-29 14:58:50 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,29 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { FaUserCircle } from "react-icons/fa";
 import { FaCloudMoon } from "react-icons/fa";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 function App() {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
     <div className="App">
       <div className="container-fluid">
@@ -41,7 +63,9 @@ function App() {
               Yoga Teacher Trainings, classes, meditation,pranayama,music ,
               Philosophy and more.
             </p>
-            <p><i>Everything under $10.</i></p>
+            <p>
+              <i>Everything under $10.</i>
+            </p>
           </div>
         </div>
         <div className="counter-container">
@@ -108,22 +132,56 @@ function App() {
         </div>
         <div className="accreditationContainer">
           <div className="w-50  accreditionLeftContainer">
-            <p className="h3"><b>Get Internationally accredited by Bodsphere, from the comfort of your home</b></p>
-            <p className="pt-5 pb-5">Join  the Bodsphere community and take the first step towards joining the world's biggest community of yoga schools and teachers that represent hight quality, safe , affordable, accissible and equitable yoga.</p>
+            <p className="h3">
+              <b>
+                Get Internationally accredited by Bodsphere, from the comfort of
+                your home
+              </b>
+            </p>
+            <p className="pt-5 pb-5">
+              Join the Bodsphere community and take the first step towards
+              joining the world's biggest community of yoga schools and teachers
+              that represent hight quality, safe , affordable, accissible and
+              equitable yoga.
+            </p>
             <button className="customButton">Get Accredited</button>
           </div>
           <div className="w-50  d-flex justify-content-center align-items-center">
-            <img src="/img1.png"/>
+            <img src="/img1.png" />
           </div>
         </div>
-        {/* <div className="sliderContainer1">
+        <div className="sliderContainer1">
           <p>Meet Your Instructor</p>
-          <p>We work with the finest professinals to ensure you feel the best.</p>
-          <div>
-
+          <p>
+            We work with the finest professinals to ensure you feel the best.
+          </p>
+          <div className="slider1">
+            <Carousel
+              swipeable={false}
+              draggable={false}
+              showDots={false}
+              responsive={responsive}
+              //ssr={true} // means to render carousel on server-side.
+              infinite={true}
+             // autoPlay={this.props.deviceType !== "mobile" ? true : false}
+              autoPlaySpeed={1000}
+              keyBoardControl={true}
+              //customTransition="all .5"
+              transitionDuration={500}
+              containerClass="carousel-container"
+              removeArrowOnDeviceType={["tablet", "mobile"]}
+              //deviceType={this.props.deviceType}
+              dotListClass="custom-dot-list-style"
+              itemClass="carousel-item-padding-40-px"
+            >
+              <div><img src="/img2.png" width={300}/><p>Name</p></div>
+              <div><img src="/img3.png" width={300}/><p>Name</p></div>
+              <div><img src="/img4.png" width={300}/><p>Name</p></div>
+              <div><img src="/img5.png" width={300}/><p>Name</p></div>
+            </Carousel>
+            ; ;
           </div>
-
-        </div> */}
+        </div>
       </div>
     </div>
   );
